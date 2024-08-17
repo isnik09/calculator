9ea4391a8e7c529126e9f252063b6ba86573dbf5
NIK <isnik@system.nik>
2024-08-17 23:44:15 +0530

    fixed issues with reset

--- js/index.js
+++ js/index.js
@@ -1,1 +1,55 @@
+const output = document.querySelector(".output");
+const buttons = document.querySelectorAll(".btn");
+const resetButton = document.querySelector(".reset");
 
+function updateOutput(value) {
+  output.textContent += value;
+}
+
+function clearOutput() {
+  output.textContent = "";
+  output.textContent = "0";
+}
+
+function deleteLastChar() {
+  output.textContent = output.textContent.slice(0, -1);
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const value = button.textContent;
+
+    switch (value) {
+      case "C":
+        clearOutput();
+        break;
+      case "=":
+        try {
+          const result = eval(output.textContent);
+          if (result === Infinity || result === -Infinity) {
+            alert("Infinity");
+            output.textContent = "0";
+          } else {
+            output.textContent = result;
+          }
+        } catch (error) {
+          alert("Error");
+          clearOutput();
+          output.textContent = "0";
+        }
+        break;
+      case "del":
+        deleteLastChar();
+        break;
+      default:
+        if (output.textContent === "0") {
+          output.textContent = "";
+        }
+        updateOutput(value);
+    }
+  });
+});
+
+resetButton.addEventListener("click", clearOutput);
+
+output.textContent = "0";
